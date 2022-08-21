a46f09fbcd74c82804a45fd3f5c93e44650658bb
Mathias Dahlgaard <mm.dahlgaard@hotmail.com>
2022-08-21 23:03:03 +0200

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Mathias Dahlgaard on 21/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
